4343c1e5259d30023c15ae38bed23033fc875c7a
Voiceflow <josh.rampersad@voiceflow.com>
2022-07-22 15:07:39 -0400

    test: build command

--- src/scripts/utils/setup-local-registry.sh
+++ src/scripts/utils/setup-local-registry.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-echo "CONFIG: $CONFIG"
-
 docker create -v /verdaccio/conf --name verdaccio-conf alpine:3.4 /bin/true
-docker cp "$CONFIG" verdaccio-conf:/verdaccio/conf
+/bin/bash -c "docker cp $CONFIG verdaccio-conf:/verdaccio/conf"
 
 docker run -it --name verdaccio --network host -e NPM_TOKEN=${NPM_TOKEN} --volumes-from verdaccio-conf verdaccio/verdaccio:5
