e9030ff7c5cfd3a3fa5b54fa1db619d4ba611690
Xavier Portilla Edo <xavi_tb@hotmail.com>
2022-08-09 16:38:21 +0200

    feat: added turborepo (IN-338) (#84)
    * feat: add turborepo
    
    * fix: orb bad missing params
    
    * feat: added turborepo engine
    
    * ci: lint fix and persist all
    
    * feat: copy all yarn lock files
    
    * ci: fix yarn lock files
    
    * feat: add next build
    
    * ci:added types tests
    
    * feat: add the ability of copy lock files based on flag
    
    * feat: add local script to validate the orb locally
    
    * feat: refactor yarn lock files
    
    * fix: yaml lint errors
    
    * feat: ad package folder to update track system
    
    * feat: move the yarn lock file to build process
    
    * ci: fix yamlint
    
    * feat: save and restore refactor
    
    * ci: fix yamlint
    
    * ci: fix cache
    
    * fix: force publish
    
    * feat: added cache identifier
    
    * fix: identifier
    
    * ci: add app arg
    
    * fix: build param
    
    * ci: set different release engines
    
    * ci: fix lint
    
    * ci: using npx
    
    * ci: fix equal
    
    * fix: remove git commit, get latest tag created
    
    * ci: not link to package folder
    
    * fix: added package folder
    
    * fix: fix when we are in a monorepo
    
    * ci: added better control on sub packages
    
    * fix: iterate over packages
    
    * fi: iterate over sub packages
    
    * fix: only enter on folders
    
    * ci: last monorepo fix
    
    * Update src/commands/monorepo/monorepo_exec_command.yml
    
    Co-authored-by: Xavientois <34867186+Xavientois@users.noreply.github.com>
    
    * fix: remove unused -e
    
    * Update src/commands/monorepo/monorepo_save_cache.yml
    
    Co-authored-by: Xavientois <34867186+Xavientois@users.noreply.github.com>
    
    * Update src/commands/track/update_track.yml
    
    Co-authored-by: Xavientois <34867186+Xavientois@users.noreply.github.com>
    
    * Update src/jobs/monorepo/monorepo_release.yml
    
    Co-authored-by: Xavientois <34867186+Xavientois@users.noreply.github.com>
    
    * Update src/commands/track/update_track.yml
    
    Co-authored-by: Xavientois <34867186+Xavientois@users.noreply.github.com>
    
    * Update src/commands/monorepo/stop_if_no_changes.yml
    
    Co-authored-by: Xavientois <34867186+Xavientois@users.noreply.github.com>
    
    * Update src/commands/monorepo/monorepo_types_tests.yml
    
    Co-authored-by: Xavientois <34867186+Xavientois@users.noreply.github.com>
    
    * Update src/commands/monorepo/monorepo_save_cache.yml
    
    Co-authored-by: Xavientois <34867186+Xavientois@users.noreply.github.com>
    
    * Update src/commands/monorepo/monorepo_restore_cache.yml
    
    Co-authored-by: Xavientois <34867186+Xavientois@users.noreply.github.com>
    
    * Update src/commands/monorepo/monorepo_save_cache.yml
    
    Co-authored-by: Xavientois <34867186+Xavientois@users.noreply.github.com>
    
    * Update src/commands/monorepo/monorepo_save_cache.yml
    
    Co-authored-by: Xavientois <34867186+Xavientois@users.noreply.github.com>
    
    * fix: copy paste issue
    
    * Update src/jobs/track/update_track.yml
    
    Co-authored-by: Xavientois <34867186+Xavientois@users.noreply.github.com>
    
    * Update src/jobs/track/check_track_exists.yml
    
    Co-authored-by: Xavientois <34867186+Xavientois@users.noreply.github.com>
    
    * Update src/commands/monorepo/monorepo_restore_cache.yml
    
    Co-authored-by: Xavientois <34867186+Xavientois@users.noreply.github.com>
    
    * Update src/commands/monorepo/monorepo_save_cache.yml
    
    Co-authored-by: Xavientois <34867186+Xavientois@users.noreply.github.com>
    
    * fix: copy paste fixed
    
    * Update src/commands/monorepo/monorepo_restore_cache.yml
    
    Co-authored-by: Xavientois <34867186+Xavientois@users.noreply.github.com>
    
    Co-authored-by: Xavientois <34867186+Xavientois@users.noreply.github.com>

---
+++ validate.sh
@@ -0,0 +1,19 @@
+
+SRC_DIR=$(pwd)/src
+
+# Flatten all the yaml files
+for DIR in commands jobs executors examples; do
+    # Copy all files in all subdirectories into orb-src
+    find $SRC_DIR/$DIR -type f -exec cp {} $SRC_DIR/$DIR \; || :
+done;
+
+# Pack orb
+circleci orb pack $SRC_DIR > "$SRC_DIR/orb.yml"
+
+# Validate orb
+circleci orb validate "$SRC_DIR/orb.yml"
+
+# Clean all copied files
+for DIR in commands jobs executors examples; do
+    find $SRC_DIR/$DIR -type f -maxdepth 1  -name "*.yml" -exec rm -rf {} \; || :
+done;
