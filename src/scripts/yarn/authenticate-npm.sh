6a578228fb97bf23626f398b4375f2e2a0264a41
Xavientois <34867186+Xavientois@users.noreply.github.com>
2022-08-16 09:43:36 -0400

    extract and fix authentication script

---
+++ src/scripts/yarn/authenticate-npm.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "//registry.npmjs.org/:_authToken=$NPM_TOKEN" >> ~/.npmrc
+cat << EOF > ~/.yarnrc.yml
+npmRegistries:
+    "https://registry.yarnpkg.com":
+    npmAuthToken: $NPM_TOKEN
+    "https://registry.npmjs.org":
+    npmAuthToken: $NPM_TOKEN
+EOF
