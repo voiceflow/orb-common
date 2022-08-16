314875b74f82a896bf586464a898d09af6b6d226
Xavientois <34867186+Xavientois@users.noreply.github.com>
2022-08-16 09:54:53 -0400

    fix indent

--- src/scripts/yarn/authenticate-npm.sh
+++ src/scripts/yarn/authenticate-npm.sh
@@ -3,8 +3,8 @@
 echo "//registry.npmjs.org/:_authToken=$NPM_TOKEN" >> ~/.npmrc
 cat << EOF > ~/.yarnrc.yml
 npmRegistries:
-    "https://registry.yarnpkg.com":
+  "https://registry.yarnpkg.com":
     npmAuthToken: $NPM_TOKEN
-    "https://registry.npmjs.org":
+  "https://registry.npmjs.org":
     npmAuthToken: $NPM_TOKEN
 EOF
