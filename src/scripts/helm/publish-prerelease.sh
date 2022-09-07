5fba96f9958f31ca0be522b7ca31ad33c068b16c
Xavientois <34867186+Xavientois@users.noreply.github.com>
2022-09-07 13:46:42 -0400

    use 0.0.0

--- src/scripts/helm/publish-prerelease.sh
+++ src/scripts/helm/publish-prerelease.sh
@@ -6,12 +6,12 @@ if [[ -d "$file" && -f "$file/$file/Chart.yaml" ]]; then
     cd $file;
     helm dep update $file;
 
-    echo "Getting current chart version"
-    VERSION="$(cat $file/Chart.yaml | yq -r '.version')"
-
     echo "Getting short SHA1 of git commit"
     SHA="$(git rev-parse --short HEAD)"
+    VERSION="0.0.0" # Use constant version for beta releases
     BETA_VERSION="${VERSION}+${SHA}"
+
+    echo "Packaging version $BETA_VERSION"
     helm package $file --version "$BETA_VERSION"
 
     echo "publishing in $channel channel";
