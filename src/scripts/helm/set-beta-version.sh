81cb60e1f612e0292c6678ece2c9d18e404d4af2
Xavientois <34867186+Xavientois@users.noreply.github.com>
2022-09-09 16:24:06 -0400

    feat: allow updating internal dependencies for beta releases (IN-121) (#96)
    * fix: trigger release for monorepo exec fix
    
    * feat: trigger release
    
    * feat: allow updating internal dependencies for beta releases
    
    * fix indent
    
    * fix script
    
    * fix script

---
+++ src/scripts/helm/set-beta-version.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Sets BETA_VERSION to contain the beta version for this commit
+
+echo "Getting short SHA1 of git commit"
+SHA="$(git rev-parse --short HEAD)"
+VERSION="0.0.0" # Use constant version for beta releases
+
+# Export to subsequent steps
+echo "export BETA_VERSION=\"${VERSION}-${SHA}\"" >> "$BASH_ENV"
