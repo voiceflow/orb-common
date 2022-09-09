0549d1349711f1cc93aef3884e20ce4dd88894f8
Xavientois <34867186+Xavientois@users.noreply.github.com>
2022-09-09 10:29:32 -0400

    feat: add helm beta release (IN-121) (#94)
    * fix: trigger release for monorepo exec fix
    
    * feat: trigger release
    
    * feat: add helm beta release
    
    * fix indentation
    
    * make job
    
    * rename job
    
    * move commit message check
    
    * extract script and use sha
    
    * add extension
    
    * remove slash
    
    * add indent
    
    * fix indent
    
    * use 0.0.0
    
    * use - instead of +
    
    * fix intentation
    
    * add comment

---
+++ src/scripts/helm/publish-prerelease.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Publishes beta releases for helm charts in subdirectories of working directory
+# Similar implementation to the `helm-publish-charts` command. If updating this script, be sure to update that one as well
+for file in * ; do
+    if [[ -d "$file" && -f "$file/$file/Chart.yaml" ]]; then
+        echo "packaging $file";
+        cd $file;
+        helm dep update $file;
+
+        echo "Getting short SHA1 of git commit"
+        SHA="$(git rev-parse --short HEAD)"
+        VERSION="0.0.0" # Use constant version for beta releases
+        BETA_VERSION="${VERSION}-${SHA}"
+
+        echo "Packaging version $BETA_VERSION"
+        helm package $file --version "$BETA_VERSION"
+
+        echo "publishing in $channel channel";
+        CHART="$file-${BETA_VERSION}.tgz"
+
+        if [ ! -f "$CHART" ]; then
+            echo "Packaged chart does not have expected name $CHART. Does the name in Chart.yaml match the directory name?"
+            exit 3
+        fi
+
+        helm s3 push --force "$CHART" voiceflow-charts-s3-beta;
+        cd ..;
+    fi;
+done
