3f74b8b0130091b3236ac0b3eb6e0efcee2253ad
Ben Teichman <ben@voiceflow.com>
2022-09-22 06:35:41 -0400

    wip: try some stuff

--- validate.sh
+++ validate.sh
@@ -1,8 +1,7 @@
-
 SRC_DIR=$(pwd)/src
 
 # Flatten all the yaml files
-for DIR in commands jobs executors examples; do
+for DIR in commands jobs executors; do
     # Copy all files in all subdirectories into orb-src
     find $SRC_DIR/$DIR -type f -exec cp {} $SRC_DIR/$DIR \; || :
 done;
@@ -14,6 +13,6 @@ circleci orb pack $SRC_DIR > "$SRC_DIR/orb.yml"
 circleci orb validate "$SRC_DIR/orb.yml"
 
 # Clean all copied files
-for DIR in commands jobs executors examples; do
+for DIR in commands jobs executors; do
     find $SRC_DIR/$DIR -type f -maxdepth 1  -name "*.yml" -exec rm -rf {} \; || :
 done;
