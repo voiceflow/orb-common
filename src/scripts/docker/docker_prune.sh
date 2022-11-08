7950917d6278ff1b74c7d4ad59522bcb20fcd173
xavidop <xavi_tb@hotmail.com>
2022-11-08 13:29:00 +0100

    fix: add echo message

--- src/scripts/docker/docker_prune.sh
+++ src/scripts/docker/docker_prune.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 if [[ $CLEAN_VOLUMES == true ]]; then
+    echo "cleaning volumes unused..."
     docker volume prune -f
 fi
 
 if [[ $CLEAN_IMAGES == true ]]; then
+    echo "cleaning images unused..."
     docker system prune -f
 fi
