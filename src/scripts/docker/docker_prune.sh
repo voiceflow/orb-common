ce6e4d421fc054c95d2aa90227b2b87d5532b613
xavidop <xavi_tb@hotmail.com>
2022-11-08 14:34:30 +0100

    fix: numeric comparison

--- src/scripts/docker/docker_prune.sh
+++ src/scripts/docker/docker_prune.sh
@@ -3,12 +3,12 @@
 echo $CLEAN_VOLUMES
 echo $CLEAN_IMAGES
 
-if [[ $CLEAN_VOLUMES == true ]]; then
+if (( $CLEAN_VOLUMES )); then
     echo "cleaning volumes unused..."
     docker volume prune -f
 fi
 
-if [[ $CLEAN_IMAGES == true ]]; then
+if (( $CLEAN_IMAGES )); then
     echo "cleaning images unused..."
     docker system prune -f
 fi
