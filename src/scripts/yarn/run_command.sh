4325919ef499bf5e065319ed85d40864ff37781a
Xavientois <34867186+Xavientois@users.noreply.github.com>
2023-01-17 09:35:33 -0500

    fix: extract and refactor yarn command (IN-304) (#126)
    * fix: extract and refactor yarn command
    
    * fix: conditionally remove lockfile
    
    * test: check yarn version
    
    * test: check yarn version
    
    * feat: bump orb version
    
    * add working directory
    
    * improve echo
    
    * remove whitespace
    
    * extract both yarn command in/out of container
    
    * extract both yarn command in/out of container
    
    * test: extracted command as env var
    
    * test: extracted command as env var in bash -c

---
+++ src/scripts/yarn/run_command.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# shellcheck disable=SC2086
+
+trap 'echo "fail detected"; touch /tmp/failure' ERR
+
+echo "Running command: ${COMMAND:?}"
+bash -c "${COMMAND:?}"
+
+if (( ${SHOULD_REMOVE_LOCKFILE?} )); then
+    echo "Removing lock file ${LOCK_FILE?}" 
+    rm -rf "${LOCK_FILE}"
+fi
---
+++ src/scripts/yarn/run_command_in_container.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+trap 'echo "fail detected"; touch /tmp/failure' ERR
+
+echo "Copying code into container"
+docker create -v /code --name code "${CONTAINER_IMAGE:?}" /bin/true
+docker cp "$PWD/." code:/code
+
+echo "Executing command \"${COMMAND:?}\" in container"
+docker run --name runner -it --volumes-from code -w /code "${CONTAINER_IMAGE:?}" /bin/bash -c "${COMMAND:?}"
+
+# If a folder is specified we copy that one on the host, otherwise copy all
+SOURCE_FOLDER="runner:/code/${FOLDER_TO_COPY:-.}"
+
+if [[ -n "${MONOREPO_PACKAGE?}" ]]; then
+    DESTINATION_FOLDER="$PWD/${MONOREPO_PACKAGE_FOLDER:?}/${MONOREPO_PACKAGE:?}"
+else
+    DESTINATION_FOLDER="$PWD"
+fi
+
+echo "Copying from ${SOURCE_FOLDER} into ${DESTINATION_FOLDER?}"
+docker cp "${SOURCE_FOLDER}" "${DESTINATION_FOLDER}"
+
+if (( ${SHOULD_REMOVE_LOCKFILE?} )); then
+    echo "Removing lock file ${LOCK_FILE?}" 
+    rm -rf "${LOCK_FILE}"
+fi
