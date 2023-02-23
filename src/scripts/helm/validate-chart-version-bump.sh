6979a058bd88a5d13f20e73b872be9c1c67576b1
Xavientois <34867186+Xavientois@users.noreply.github.com>
2023-02-23 11:37:40 -0500

    add get changed files

--- src/scripts/helm/validate-chart-version-bump.sh
+++ src/scripts/helm/validate-chart-version-bump.sh
@@ -2,10 +2,4 @@
 
 for chart in ${CHARTS?}; do
   echo "Checking if chart ${chart} has been modified"
-  if [[ -n $(git diff --name-only origin/master -- ${chart}/Chart.yaml) ]]; then
-    if [[ -n $(git diff --name-only origin/master -- ${chart}/Chart.yaml | grep -v version) ]]; then
-      echo "Chart ${chart} has been modified but version has not been bumped"
-      exit 1
-    fi
-  fi
 done
