c6c5877116f8a2de3e3f68159a7ffd06ad2019b3
Xavientois <34867186+Xavientois@users.noreply.github.com>
2023-02-24 08:43:02 -0500

    feat: add chart version bump validation (IN-631) (#132)
    * feat: add chart version bump validation
    
    * add get changed files
    
    * add get changed files
    
    * implement version check
    
    * implement version check
    
    * implement version check
    
    * extract modified charts
    
    * extract modified charts
    
    * extract modified charts
    
    * use extracted
    
    * use extracted
    
    * use extracted
    
    * use extracted
    
    * use extracted
    
    * use extracted
    
    * use extracted
    
    * use extracted
    
    * use extracted
    
    * rename file change step
    
    * rename file change step
    
    * rename working dir to chart dir
    
    * respond to review

---
+++ src/scripts/helm/get_modified_charts.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Expected environment variables:
+echo "CHANGED_FILES: ${CHANGED_FILES?}"
+echo "CHART_DIR: ${CHART_DIR:?}"
+echo "MODIFIED_CHARTS_ENV: ${MODIFIED_CHARTS_ENV:?}"
+
+# Get the list of all charts
+ALL_CHARTS=()
+for DIR in * ; do
+    if [[ -d "$DIR" && -f "$DIR/$DIR/Chart.yaml" ]]; then
+    ALL_CHARTS+=("$DIR")
+    fi;
+done
+
+# Extract only modified charts
+MODIFIED_CHARTS=()
+for CHART in "${ALL_CHARTS[@]}"; do
+    if grep -q -oP "M\s*${CHART_DIR:?}/${CHART}/${CHART}/.*" <<< "${CHANGED_FILES?}"; then
+    MODIFIED_CHARTS+=("$CHART")
+    fi;
+done
+
+echo "${MODIFIED_CHARTS[@]}"
+
+# shellcheck disable=SC2145
+echo "export ${MODIFIED_CHARTS_ENV:?}=\"${MODIFIED_CHARTS[@]}\"" >> "$BASH_ENV"
---
+++ src/scripts/helm/validate-chart-version-bump.sh
@@ -0,0 +1,28 @@
+#!/bin/bash -e
+
+# Expected environment variables:
+echo "CHARTS: ${CHARTS?}"
+
+rc=0 # return code
+
+for CHART in ${CHARTS?}; do
+  echo "Checking if chart ${CHART} has been modified"
+  LOCAL_CHART="$(helm show chart "$CHART/$CHART")"
+  CHANNEL="$(yq --raw-output '.annotations."release-repository"' <<< "$LOCAL_CHART")"
+
+  REPO="voiceflow-charts-s3-$CHANNEL"
+  if [[ "$CHANNEL" == "public" ]]; then
+    REPO="voiceflow-charts-s3"
+  fi
+
+  LOCAL_VERSION="$(yq --raw-output .version <<< "$LOCAL_CHART")"
+  REMOTE_VERSION="$(helm show chart "$REPO/$CHART" | yq --raw-output .version)"
+
+  # To ensure version bump, we check if the local version is greater than the remote version
+  if echo -e "$LOCAL_VERSION\n$REMOTE_VERSION" | sort -c -V 2> /dev/null; then
+    echo "ERROR: Chart version for $CHART has not been updated. Master is at $REMOTE_VERSION while this branch is at $LOCAL_VERSION" >&2
+    rc=1
+  fi
+done
+
+exit $rc
