60c91af45a34cb222a82a2782e1458c307c604e2
Xavientois <34867186+Xavientois@users.noreply.github.com>
2023-03-02 13:01:13 -0500

    feat: reindex changed charts

--- src/scripts/helm/get_modified_charts.sh
+++ src/scripts/helm/get_modified_charts.sh
@@ -4,6 +4,7 @@
 echo "CHANGED_FILES: ${CHANGED_FILES?}"
 echo "CHART_DIR: ${CHART_DIR:?}"
 echo "MODIFIED_CHARTS_ENV: ${MODIFIED_CHARTS_ENV:?}"
+echo "MODIFIED_CHARTS_REPOS_ENV: ${MODIFIED_CHARTS_REPOS_ENV:?}"
 
 # Add trailing slash to CHART_DIR if missing
 if [[ "$CHART_DIR" != */ ]]; then
@@ -25,13 +26,28 @@ done
 
 # Extract only modified charts
 MODIFIED_CHARTS=()
+MODIFIED_CHARTS_REPOS=()
 for CHART in "${ALL_CHARTS[@]}"; do
     if grep -q -oP "M\s*${CHART_DIR}${CHART}/${CHART}/.*" <<< "${CHANGED_FILES?}"; then
-    MODIFIED_CHARTS+=("$CHART")
+        MODIFIED_CHARTS+=("$CHART")
+
+        # Also keep track of channel for re-indexing
+        CHANNEL=$(helm show chart "${CHART}/${CHART}" | yq --raw-output '.annotations."release-repository"')
+        REPO="voiceflow-charts-s3-$CHANNEL"
+        if [[ "$CHANNEL" == "public" ]]; then
+            REPO="voiceflow-charts-s3"
+        fi
+
+        MODIFIED_CHARTS_REPOS+=("$REPO")
     fi;
 done
 
+# Ensure each repo is unique
+MODIFIED_CHARTS_REPOS="$(echo "${MODIFIED_CHARTS_REPOS[@]}" | tr ' ' '\n' | sort -u | tr '\n' ' ')"
+
 echo "${MODIFIED_CHARTS[@]}"
+echo "${MODIFIED_CHARTS_REPOS}"
 
 # shellcheck disable=SC2145
 echo "export ${MODIFIED_CHARTS_ENV:?}=\"${MODIFIED_CHARTS[@]}\"" >> "$BASH_ENV"
+echo "export ${MODIFIED_CHARTS_REPOS_ENV:?}=\"${MODIFIED_CHARTS_REPOS}\"" >> "$BASH_ENV"
