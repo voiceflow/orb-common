c2c56a71b5ff1f60c09e9c93bbfcf25b54095705
Xavientois <34867186+Xavientois@users.noreply.github.com>
2023-03-03 13:59:54 -0500

    fix: lints

--- src/scripts/e2e/install-vfcli.sh
+++ src/scripts/e2e/install-vfcli.sh
@@ -6,7 +6,7 @@ echo "OS: ${OS:?}"
 echo "ARCH: ${ARCH:?}"
 
 API_URL="https://${GITHUB_TOKEN:?}:@api.github.com/repos/voiceflow/vfcli"
-ASSET_ID="$(curl $API_URL/releases/${VERSION:?} | jq -r ".assets[] | select(.name | contains(\"${OS:?}_${ARCH:?}\")) | .id")"
+ASSET_ID="$(curl "$API_URL/releases/${VERSION:?}" | jq -r ".assets[] | select(.name | contains(\"${OS:?}_${ARCH:?}\")) | .id")"
 curl -J -L -H "Accept: application/octet-stream" "$API_URL/releases/assets/${ASSET_ID:?}" --output vfcli.tar.gz
 tar -xf vfcli.tar.gz
 
--- src/scripts/vfcli/fetch-endpoints.sh
+++ src/scripts/vfcli/fetch-endpoints.sh
@@ -5,7 +5,7 @@ echo "CLUSTER: ${CLUSTER:?}"
 echo "COMPONENTS: ${COMPONENTS?}"
 echo "ENV_NAME: ${ENV_NAME:?}"
 
-vfcli init --interactive false --no-telepresence --cluster ${CLUSTER:?}
+vfcli init --interactive false --no-telepresence --cluster "${CLUSTER:?}"
 
 COMPONENT_DATA="$(vfcli component list --name "${ENV_NAME:?}" --full --output json --interactive false)"
 
@@ -13,6 +13,6 @@ for COMPONENT in ${COMPONENTS?}; do
     echo "Fetching $COMPONENT URL"
     echo "${COMPONENT_DATA}" | jq --raw-output ".[] | select(.id == \"${COMPONENT}\") | .endpoint" > "${COMPONENT}.url"
     ENV_VAR_NAME=$(echo "${COMPONENT^^}_URL" | tr '-' '_')
-    echo "Storing url for $COMPONENT in $ENV_VAR_NAME: $(cat ${COMPONENT}.url)"
-    echo "export ${ENV_VAR_NAME}=\"$(cat ${COMPONENT}.url)\"" >> "$BASH_ENV"
+    echo "Storing url for $COMPONENT in $ENV_VAR_NAME: $(cat "${COMPONENT}.url")"
+    echo "export ${ENV_VAR_NAME}=\"$(cat "${COMPONENT}.url")\"" >> "$BASH_ENV"
 done
