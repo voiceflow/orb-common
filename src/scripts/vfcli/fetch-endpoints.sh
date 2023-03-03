9ffbe3a15b22d3d1cc852f54998e3e6de84a95d9
Xavientois <34867186+Xavientois@users.noreply.github.com>
2023-03-03 13:57:34 -0500

    feat: add fetch-endpoints script

--- src/scripts/vfcli/fetch-endpoints.sh
+++ src/scripts/vfcli/fetch-endpoints.sh
@@ -7,9 +7,11 @@ echo "ENV_NAME: ${ENV_NAME:?}"
 
 vfcli init --interactive false --no-telepresence --cluster ${CLUSTER:?}
 
+COMPONENT_DATA="$(vfcli component list --name "${ENV_NAME:?}" --full --output json --interactive false)"
+
 for COMPONENT in ${COMPONENTS?}; do
     echo "Fetching $COMPONENT URL"
-    vfcli component list --name "${ENV_NAME:?}" --full --output json --interactive false | jq --raw-output ".[] | select(.id == \"${COMPONENT}\") | .endpoint" > "${COMPONENT}.url"
+    echo "${COMPONENT_DATA}" | jq --raw-output ".[] | select(.id == \"${COMPONENT}\") | .endpoint" > "${COMPONENT}.url"
     ENV_VAR_NAME=$(echo "${COMPONENT^^}_URL" | tr '-' '_')
     echo "Storing url for $COMPONENT in $ENV_VAR_NAME: $(cat ${COMPONENT}.url)"
     echo "export ${ENV_VAR_NAME}=\"$(cat ${COMPONENT}.url)\"" >> "$BASH_ENV"
