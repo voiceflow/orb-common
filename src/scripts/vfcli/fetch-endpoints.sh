8a5d5687d9bda598a9a093bc3eede91d1923a759
Xavientois <34867186+Xavientois@users.noreply.github.com>
2023-03-21 16:33:40 -0400

    fix: init on install

--- src/scripts/vfcli/fetch-endpoints.sh
+++ src/scripts/vfcli/fetch-endpoints.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
 # Expected environment variables:
-echo "CLUSTER: ${CLUSTER:?}"
 echo "COMPONENTS: ${COMPONENTS?}"
 echo "ENV_NAME: ${ENV_NAME:?}"
-
-vfcli init --interactive false --no-telepresence --cluster "${CLUSTER:?}"
 
 COMPONENT_DATA="$(vfcli component list --name "${ENV_NAME:?}" --full --output json --interactive false)"
 
