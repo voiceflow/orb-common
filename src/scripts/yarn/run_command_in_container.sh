904f5800db28cf9fa02594c9b8ea7dba1da1d095
Greg O'Grady <gregory.ogrady@voiceflow.com>
2024-02-01 11:16:51 -0500

    wip: wip

--- src/scripts/yarn/run_command_in_container.sh
+++ src/scripts/yarn/run_command_in_container.sh
@@ -2,7 +2,7 @@
 
 trap 'echo "fail detected"; touch /tmp/failure' ERR
 
-SRC_ROOT=/tmp/code
+SRC_ROOT=/code
 # If a folder is specified we copy that one on the host, otherwise copy all
 SOURCE_FOLDER="${SRC_ROOT}/${FOLDER_TO_COPY:-.}"
 
@@ -12,13 +12,24 @@ else
     DESTINATION_FOLDER="$PWD"
 fi
 
-echo "Copying from ${SOURCE_FOLDER} into ${DESTINATION_FOLDER?}"
-echo "Copying code into container"
+echo "SOURCE_FOLDER: ${SOURCE_FOLDER}"
+pwd && ls -lah .
+echo "DESTINATION_FOLDER: ${DESTINATION_FOLDER}"
+ls -lah "${DESTINATION_FOLDER}"
+
 echo "Executing command \"${COMMAND:?}\" in container \"${CONTAINER_IMAGE:?}\""
-docker run --rm -i -v "${PWD}":/src -v "${DESTINATION_FOLDER}":/out --entrypoint /bin/sh "${CONTAINER_IMAGE:?}" <<EOF
+docker run --rm -i -v ./:/src -v "${DESTINATION_FOLDER}":/out --entrypoint /bin/sh "${CONTAINER_IMAGE:?}" <<EOF
     echo "Copying /src to ${SRC_ROOT}"
+    echo "ls /src"
+    ls -lah /src
+
+    echo "ls /out"
+    ls -lah /out
+
     cp -R /src ${SRC_ROOT}
     cd ${SRC_ROOT}
+    pwd && ls -lah .
+
     rm -rf /tmp/.success
     for _ in {0..${MAX_RETRIES:?}}; do
         if /bin/sh -c "${COMMAND?}"; then
@@ -30,7 +41,11 @@ docker run --rm -i -v "${PWD}":/src -v "${DESTINATION_FOLDER}":/out --entrypoint
         echo "Retrying command: ${COMMAND?}"
     done
 
+    echo "Copying from ${SOURCE_FOLDER} into ${DESTINATION_FOLDER?}"
+    echo "ls ${SRC_ROOT}"
+    ls -lah "${SRC_ROOT}"
     cp -R "${SOURCE_FOLDER}" /out
+    ls -lah /out
 
     # Success: clean exit
     ls -lah /tmp
@@ -39,6 +54,8 @@ docker run --rm -i -v "${PWD}":/src -v "${DESTINATION_FOLDER}":/out --entrypoint
     echo "failed: ${COMMAND?}" >&2
     exit 1
 EOF
+
+ls -lah "${DESTINATION_FOLDER}"
 
 if (( ${SHOULD_REMOVE_LOCKFILE?} )); then
     echo "Removing lock file ${LOCK_FILE?}" 
