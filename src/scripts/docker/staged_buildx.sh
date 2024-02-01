c9c0fdcb3441c63935c0d317d3e951669b29e794
Greg O'Grady <gregory.ogrady@voiceflow.com>
2024-02-01 15:12:16 -0500

    feat: docker buildx for target stages

---
+++ src/scripts/docker/staged_buildx.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+# Expected env vars
+echo "TARGET: ${TARGET:=prod}"
+echo "IMAGE_REPO: ${IMAGE_REPO?}"
+echo "IMAGE_TAG: ${IMAGE_TAG:=${TARGET}}"
+echo "PLATFORMS: ${PLATFORMS:=linux/amd64}"
+echo "DOCKERFILE: ${DOCKERFILE:=Dockerfile}"
+
+# NOTE: think of this as the CircleCI DLC key
+echo "BUILDER: ${BUILDER:=buildy}"
+
+# This is specific
+echo "NPM_TOKEN: ${NPM_TOKEN#"//registry.npmjs.org/:_authToken="}"
+# TODO: make file secret ~/.yarnrc.yml,target=$HOME/.yarnrc.yml
+# TODO: extensible secrets and build args
+
+docker buildx inspect "${BUILDER}" >/dev/null 2>&1 || docker buildx create --platform="${PLATFORMS}" --name "${BUILDER}"
+docker buildx use "${BUILDER}"
+docker buildx inspect --bootstrap
+
+docker buildx build . \
+  -f "${DOCKERFILE}" \
+  -t "${IMAGE_REPO}:${IMAGE_TAG}" \
+  --secret id=NPM_TOKEN \
+  --target "${TARGET}" \
+  --platform "${PLATFORMS}" \
+  --load
