42f1aaa160240035468be5a1d0d48f1ea38a38a0
Greg O'Grady <gregory.ogrady@voiceflow.com>
2024-03-08 12:29:26 -0500

    feat: cache-from/cache-to params

--- src/scripts/docker/staged_buildx.sh
+++ src/scripts/docker/staged_buildx.sh
@@ -8,6 +8,7 @@ echo "PLATFORMS: ${PLATFORMS:=linux/amd64}"
 echo "DOCKERFILE: ${DOCKERFILE:=Dockerfile}"
 echo "NO_CACHE_FILTER: ${NO_CACHE_FILTER:=prod}"
 echo "CLEANUP_IMAGE: ${CLEANUP_IMAGE:=0}"
+echo "ENABLE_CACHE_TO: ${ENABLE_CACHE_TO:=0}"
 
 # TODO: should be general build-args
 echo "PACKAGE: ${PACKAGE-}"
@@ -17,7 +18,8 @@ echo "PACKAGE: ${PACKAGE-}"
 echo "BUILDER: ${BUILDER:=buildy}"
 
 # shellcheck source=/dev/null
-for i in /tmp/vf-staged_buildx-*.env_var ; do source "$i" ; done
+test -n "$(shopt -s nullglob; echo /tmp/vf-staged_buildx-*.env_var)" \
+  && for i in /tmp/vf-staged_buildx-*.env_var ; do source "$i" ; done
 echo "EXTRA_BUILD_ARGS: ${EXTRA_BUILD_ARGS[*]}"
 
 if [ -z "${OUTPUT-}" ] ; then
@@ -28,13 +30,33 @@ fi
 
 echo "OUTPUT: ${OUTPUT_ARG[*]}"
 
+if [[ -z "$CIRCLE_BRANCH" && -n "$CIRCLE_TAG" ]]; then
+    BRANCH_NAME="master"
+else
+    BRANCH_NAME="${CIRCLE_BRANCH//[^[:alnum:]]/-}" # Change all non alphanumeric characters to -
+fi
+
+IMAGE_CACHE_TAG_BASE="${IMAGE_REPO-}:cache-${BRANCH_NAME-}"
+
+CACHE_FROM_ARG=(--cache-from "${IMAGE_REPO-}:cache-master")
+
+if [ "${BRANCH_NAME}" != "master" ] ; then
+  CACHE_FROM_ARG+=(--cache-from "${IMAGE_REPO-}:cache-${BRANCH_NAME-}")
+fi
+
+if [ "${ENABLE_CACHE_TO-}" = "true" ] ; then
+  CACHE_TO_ARG=(--cache-to "mode=max,image-manifest=true,oci-mediatypes=true,type=registry,ref=${IMAGE_CACHE_TAG_BASE}-${IMAGE_TAG-}")
+fi
+
+
 # This is specific
 echo "NPM_TOKEN: ${NPM_TOKEN#"//registry.npmjs.org/:_authToken="}"
 # TODO: make file secret ~/.yarnrc.yml,target=$HOME/.yarnrc.yml
 # TODO: extensible secrets and build args
 
 docker buildx inspect "${BUILDER}" >/dev/null 2>&1 || docker buildx create --platform="${PLATFORMS}" --name "${BUILDER}"
-docker buildx inspect --bootstrap --use "${BUILDER}"
+docker buildx use "${BUILDER-}"
+docker buildx inspect --bootstrap
 
 if [[ -n "$PACKAGE" ]]; then
     PACKAGE_ARG=(--build-arg APP_NAME="$PACKAGE")
@@ -45,6 +67,8 @@ docker buildx build . \
   -t "${IMAGE_REPO}:${IMAGE_TAG}" \
   "${PACKAGE_ARG[@]}" \
   "${EXTRA_BUILD_ARGS[@]}" \
+  "${CACHE_FROM_ARG[@]}" \
+  "${CACHE_TO_ARG[@]}" \
   --secret id=NPM_TOKEN \
   --target "${TARGET}" \
   --platform "${PLATFORMS}" \
