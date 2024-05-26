5991535c1a3e69eeb5487387614dd90f0b20b6aa
Ose Dania <osegbemoh.dania@voiceflow.com>
2024-05-26 20:34:58 +0200

    comment out unused variables

--- src/scripts/helm/set-beta-version.sh
+++ src/scripts/helm/set-beta-version.sh
@@ -3,8 +3,8 @@
 # Sets BETA_VERSION to contain the beta version for this commit
 
 echo "Getting short SHA1 of git commit"
-SHA="$(git rev-parse --short HEAD)"
-VERSION="0.0.0" # Use constant version for beta releases
+# SHA="$(git rev-parse --short HEAD)"
+# VERSION="0.0.0" # Use constant version for beta releases
 
 # Export to subsequent steps
 # echo "export BETA_VERSION=\"${VERSION}-${SHA}\"" >> "$BASH_ENV"
