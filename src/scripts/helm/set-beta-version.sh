a4f6ba8867998f7e49158c2cff39817a00af43e7
Ose Dania <osegbemoh.dania@voiceflow.com>
2024-05-26 20:33:04 +0200

    hardversion beta version for testing semver reconciliation [pre-release]

--- src/scripts/helm/set-beta-version.sh
+++ src/scripts/helm/set-beta-version.sh
@@ -7,4 +7,6 @@ SHA="$(git rev-parse --short HEAD)"
 VERSION="0.0.0" # Use constant version for beta releases
 
 # Export to subsequent steps
-echo "export BETA_VERSION=\"${VERSION}-${SHA}\"" >> "$BASH_ENV"
+# echo "export BETA_VERSION=\"${VERSION}-${SHA}\"" >> "$BASH_ENV"
+
+echo "export BETA_VERSION=\"1.0.276\"" >> "$BASH_ENV"
