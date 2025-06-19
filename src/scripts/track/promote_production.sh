c89a5112edcfc2dfcc486002d9541109b25048b3
theProf <theProf@gerggle.com>
2025-06-19 15:16:16 -0400

    feat: promote_production pattern (IN-2854) (#347)
    ## Description
    Promote tags built in master to production, instead of rebuilding

---
+++ src/scripts/track/promote_production.sh
@@ -0,0 +1,108 @@
+#!/bin/bash
+
+echo "COMPONENT_NAMES: ${COMPONENT_NAMES[*]}"
+
+IMAGE_TAG="k8s-master-${CIRCLE_SHA1}"
+< <(echo "$COMPONENT_NAMES") read -r -a COMPONENT_NAMES
+
+RESULT=0
+DIGESTS=()
+DEBUG_LOG_FILE="/tmp/promote-production.err"
+
+get_digest() {
+  local SERVICE
+  local IMAGE_NAME
+  local DIGEST
+
+  SERVICE="${1?}"
+  IMAGE_NAME="${IMAGE_REGISTRY}/${SERVICE}:${IMAGE_TAG}"
+  printf "Check existence of %s..." "${IMAGE_NAME}" >&2
+  DIGEST=$(crane digest "${IMAGE_NAME}" 2>&3)
+  RESULT="$?"
+  if ((RESULT)); then
+    printf "fail\n" >&2
+    return 1
+  else
+    printf "success\n" >&2
+    echo "$DIGEST"
+  fi
+}
+
+check_exists() {
+  local NAME
+  local DIGEST
+
+  DIGESTS=()
+
+  exec 3<>"${DEBUG_LOG_FILE?}"
+
+  set +e
+  for i in "${!COMPONENT_NAMES[@]}"; do
+    NAME="${COMPONENT_NAMES[$i]}"
+    DIGEST=$(get_digest "${NAME}")
+    RESULT=$((RESULT + $?))
+    DIGESTS+=("$DIGEST")
+  done
+  set -e
+
+  exec 3>&-
+
+  if ((RESULT)); then
+    echo "ERROR: Failed to find all image tags" >&2
+    cat "${DEBUG_LOG_FILE?}"
+    exit 1
+  fi
+}
+
+add_production_tags() {
+  local SERVICE
+  local IMAGE_NAME
+
+  SERVICE="${1?}"
+  IMAGE_NAME="${IMAGE_REGISTRY}/${SERVICE}:${IMAGE_TAG}"
+
+  crane tag "${IMAGE_NAME}" "latest-production"
+  crane tag "${IMAGE_NAME}" "k8s-production-${CIRCLE_SHA1}"
+}
+
+update_track() {
+  local COMPONENT
+  local IMAGE_DIGEST
+  local TRACK
+
+  COMPONENT="${1?}"
+  IMAGE_DIGEST="${2?}"
+  TRACK="tracks/${COMPONENT}/${CIRCLE_BRANCH}"
+
+  ### update the track
+  echo "TRACK: $TRACK"
+
+  mkdir -p "$(dirname "/tmp/$TRACK")"
+
+  if [[ "$COMPONENT" = "database-cli" ]]; then
+    echo "New version published: ${IMAGE_TAG}"
+    echo "${IMAGE_TAG}" >"/tmp/${TRACK}"
+  else
+    cat <<EOF >"/tmp/${TRACK}"
+    ${COMPONENT}:
+    image:
+    tag: ${IMAGE_TAG}
+    sha: ${IMAGE_DIGEST#sha256:}
+EOF
+  fi
+  aws s3 cp "/tmp/${TRACK}" "s3://$BUCKET/$TRACK"
+}
+
+check_exists
+
+echo "All tags exist"
+echo "Adding production tag and updating tracks for..."
+
+for INDEX in "${!COMPONENT_NAMES[@]}"; do
+  NAME="${COMPONENT_NAMES[$INDEX]}"
+  DIGEST="${DIGESTS[$INDEX]}"
+
+  echo "${NAME}:${DIGEST}"
+  add_production_tags "${NAME}"
+  update_track "${NAME}" "${DIGEST}"
+done
