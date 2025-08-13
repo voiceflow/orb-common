9d54cce019e1712b1d749c78acd46190358eb936
theProf <theProf@gerggle.com>
2025-08-13 17:49:03 -0400

    wip: wip

--- src/scripts/track/promote_production.sh
+++ src/scripts/track/promote_production.sh
@@ -1,121 +1,163 @@
 #!/bin/bash
+set -eE
 
+IMAGE_REGISTRY="legit.thing"
+BUCKET="com.voiceflow.ci.assets"
+
+DEBUG=1
+DEBUG_LOG_FILE="/tmp/loggy"
+
+< <(echo "$COMPONENT_NAMES") read -r -a COMPONENT_NAMES
 echo "COMPONENT_NAMES: ${COMPONENT_NAMES[*]}"
 
-IMAGE_TAG="latest-master"
-< <(echo "$COMPONENT_NAMES") read -r -a COMPONENT_NAMES
+if ((DEBUG)); then
+  rm -f /tmp/loggy
+  # set -x
 
-RESULT=0
-DIGESTS=()
-DEBUG_LOG_FILE="/tmp/promote-production.err"
+  ##
+  # ~/workspace/work/tools/oci-registry
+  # dbuild --output type=image,push=true,registry.insecure=true,name=host.docker.internal:5002/creator-api:k8s-test
+  ##
 
-get_digest() {
-  local SERVICE
-  local IMAGE_NAME
-  local DIGEST
-
-  SERVICE="${1?}"
-  IMAGE_NAME="${IMAGE_REGISTRY}/${SERVICE}:${IMAGE_TAG}"
-  printf "Check existence of %s..." "${IMAGE_NAME}" >&2
-  DIGEST=$(crane digest "${IMAGE_NAME}" 2>&3)
-  RESULT="$?"
-  if ((RESULT)); then
-    printf "fail\n" >&2
-    return 1
-  else
-    printf "success\n" >&2
-    echo "$DIGEST"
-  fi
-}
-
-check_exists() {
-  local NAME
-  local DIGEST
-
-  DIGESTS=()
+  IMAGE_REGISTRY="localhost:5000"
+  ./configure-aws.sh
 
   exec 3<>"${DEBUG_LOG_FILE?}"
+  awscli() {
+    echo "$@" >&3
+    aws --endpoint-url http://localhost:9000 "$@" >&2
+  }
 
-  set +e
-  for i in "${!COMPONENT_NAMES[@]}"; do
-    NAME="${COMPONENT_NAMES[$i]}"
-    DIGEST=$(get_digest "${NAME}")
-    RESULT=$((RESULT + $?))
-    DIGESTS+=("$DIGEST")
+  for COMPONENT in "${COMPONENT_NAMES[@]}"; do
+    printf "[debug] setting mocks for %s...\n" "${COMPONENT}" >&3
+    printf "[debug]   creating track..." >&3
+    TRACK_PATH="tracks/${COMPONENT}/master"
+
+    awscli s3 cp - "s3://${BUCKET}/${TRACK_PATH}" <<EOF
+$COMPONENT:
+  image:
+    tag: k8s-test
+    sha: asdf23423
+EOF
+    echo "[debug]   done" >&3
+
+    printf "[debug]   creating oci repo and image..." >&3
+
+    docker buildx build \
+      --builder buildy \
+      --platform linux/arm64 \
+      --output "type=image,push=true,registry.insecure=true,name=host.docker.internal:5000/$COMPONENT:k8s-test" \
+      -f - . <<EOF
+FROM alpine:3
+RUN echo "$COMPONENT" >/tmp/whoami
+EOF
+    echo "[debug]   done" >&3
   done
-  set -e
 
-  exec 3>&-
+  #   aws() {
+  #     shift 2
+  #     COMPONENT="${1#s3://"${BUCKET}"/tracks/}"
+  #     COMPONENT="${COMPONENT%/master}"
+  #     cat <<EOF >"${2?}"
+  # $COMPONENT:
+  #   image:
+  #     tag: k8s-test
+  #     sha: asdf23423
+  # EOF
+  #     echo "[aws] args: $*" >&3
+  #   }
+  # trap 'caller 1' ERR
+  trap 'echo -e "\n$BASH_SOURCE:$LINENO: error: " ; cat "${DEBUG_LOG_FILE}"; caller 1 ; exec 3>&-' ERR
+fi
 
-  if ((RESULT)); then
-    echo "ERROR: Failed to find all image tags" >&2
-    cat "${DEBUG_LOG_FILE?}"
-    exit 1
+TMP_DIR="$(mktemp -d)"
+
+((DEBUG)) && echo "TMP_DIR=${TMP_DIR}"
+
+get_master_tracks() {
+  local DIR
+
+  DIR="${1?}"
+  shift 1
+
+  printf "Fetching tracks to %s...\n" "${DIR}"
+
+  for COMPONENT in "$@"; do
+    TRACK_PATH="tracks/${COMPONENT}/master"
+
+    printf "  %s..." "$COMPONENT"
+
+    mkdir -p "$(dirname "${DIR}/${TRACK_PATH}")"
+
+    awscli s3 cp "s3://${BUCKET}/${TRACK_PATH}" "${DIR}/${TRACK_PATH}" >&3
+    printf "done\n"
+  done
+  echo "complete"
+}
+
+parse_tag() {
+  local DIR
+  local COMPONENT
+  local TRACK_PATH_ABSOLUTE
+
+  DIR="${1?}"
+  shift 1
+  COMPONENT="${1?}"
+
+  TRACK_PATH_ABSOLUTE="${DIR}/tracks/${COMPONENT}/master"
+
+  if [[ "${COMPONENT}" == "database-cli" ]]; then
+    cat "${TRACK_PATH_ABSOLUTE}"
+  else
+    yq ".[\"$NAME\"].image.tag" "${TRACK_PATH_ABSOLUTE}"
   fi
 }
 
 add_production_tags() {
   local SERVICE
+  local IMAGE_TAG
   local IMAGE_NAME
 
   SERVICE="${1?}"
+  IMAGE_TAG="${2?}"
+
   IMAGE_NAME="${IMAGE_REGISTRY}/${SERVICE}:${IMAGE_TAG}"
 
   crane tag "${IMAGE_NAME}" "latest-production"
-  crane tag "${IMAGE_NAME}" "k8s-production-${CIRCLE_SHA1}"
 
   if [[ "$SERVICE" = "database-cli" ]]; then
     crane tag "${IMAGE_NAME}" "latest"
   fi
 }
 
-update_track() {
+copy_track() {
+  local DIR
   local COMPONENT
-  local IMAGE_DIGEST
-  local TRACK_PATH
-  local BUCKET
-  local TRACK
-  BUCKET="com.voiceflow.ci.assets"
+  local MASTER_TRACK
+  local PRODUCTION_TRACK
 
-  COMPONENT="${1?}"
-  IMAGE_DIGEST="${2?}"
-  TRACK="${CIRCLE_BRANCH}"
-
-  if [[ -z "${CIRCLE_BRANCH}" && -n "${CIRCLE_TAG}" ]]; then
-    TRACK="production"
-  fi
-
-  TRACK_PATH="tracks/${COMPONENT}/${TRACK}"
+  DIR="${1?}"
+  COMPONENT="${2?}"
 
   ### update the track
-  echo "TRACK_PATH: ${TRACK_PATH}"
-
-  mkdir -p "$(dirname "/tmp/${TRACK_PATH}")"
-
-  if [[ "$COMPONENT" = "database-cli" ]]; then
-    echo "New version published: ${IMAGE_TAG}"
-    echo "${IMAGE_TAG}" >"/tmp/${TRACK_PATH}"
-  else
-    cat <<EOF >"/tmp/${TRACK_PATH}"
-${COMPONENT}:
-  image:
-    tag: ${IMAGE_TAG}
-    sha: ${IMAGE_DIGEST#sha256:}
-EOF
-  fi
-  aws s3 cp "/tmp/${TRACK_PATH}" "s3://${BUCKET}/${TRACK_PATH}"
+  MASTER_TRACK="${DIR}/tracks/${COMPONENT}/master"
+  PRODUCTION_TRACK="tracks/${COMPONENT}/production"
+  echo "testcontainers for aws/s3"
+  printf "Copying %s master track from %s to production..." "${COMPONENT}" "${DIR}"
+  awscli s3 cp "${MASTER_TRACK}" "s3://${BUCKET}/${PRODUCTION_TRACK}"
+  echo "done"
 }
 
-check_exists
+get_master_tracks "${TMP_DIR}" "${COMPONENT_NAMES[@]}" >&2
 
-echo "All tags exist"
 echo "Adding production tag and updating tracks for..."
 
 for INDEX in "${!COMPONENT_NAMES[@]}"; do
   NAME="${COMPONENT_NAMES[$INDEX]}"
-  DIGEST="${DIGESTS[$INDEX]}"
+  TAG="$(parse_tag "$TMP_DIR" "${NAME}")"
 
-  echo "${NAME}:${DIGEST}"
-  add_production_tags "${NAME}"
-  update_track "${NAME}" "${DIGEST}"
+  echo "  ${NAME}:${TAG}"
+  add_production_tags "${NAME}" "${TAG}" >&2
+  copy_track "${TMP_DIR}" "${NAME}" >&2
 done
+exec 3>&-
