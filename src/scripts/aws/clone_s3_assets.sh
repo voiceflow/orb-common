56265b0f2b0e6a9dbe99cb16b0d841a20ab6a556
xavidop <xavi_tb@hotmail.com>
2025-10-23 19:12:13 +0200

    fix: remove debug

--- src/scripts/aws/clone_s3_assets.sh
+++ src/scripts/aws/clone_s3_assets.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
 
-set -x
-echo "CACHE_CONTROL: $CACHE_CONTROL"
-
 if (( CLEAN_DESTINATION )); then
     if [[ -n "$CACHE_CONTROL" ]]; then
-        aws s3 sync "$FROM" "$TO" --delete --cache-control "$CACHE_CONTROL"
+        aws s3 sync "$FROM" "$TO" --delete --cache-control "'$CACHE_CONTROL'"
     else
         aws s3 sync "$FROM" "$TO" --delete
     fi
 else
     if [[ -n "$CACHE_CONTROL" ]]; then
-        aws s3 sync "$FROM" "$TO" --cache-control "$CACHE_CONTROL"
+        aws s3 sync "$FROM" "$TO" --cache-control "'$CACHE_CONTROL'"
     else
         aws s3 sync "$FROM" "$TO"
     fi
