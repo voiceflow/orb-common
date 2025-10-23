1f99f587fb95a10af3d02b7865c95a7478284aa8
xavidop <xavi_tb@hotmail.com>
2025-10-23 19:12:36 +0200

    fix: remove single quotes

--- src/scripts/aws/clone_s3_assets.sh
+++ src/scripts/aws/clone_s3_assets.sh
@@ -2,13 +2,13 @@
 
 if (( CLEAN_DESTINATION )); then
     if [[ -n "$CACHE_CONTROL" ]]; then
-        aws s3 sync "$FROM" "$TO" --delete --cache-control "'$CACHE_CONTROL'"
+        aws s3 sync "$FROM" "$TO" --delete --cache-control "$CACHE_CONTROL"
     else
         aws s3 sync "$FROM" "$TO" --delete
     fi
 else
     if [[ -n "$CACHE_CONTROL" ]]; then
-        aws s3 sync "$FROM" "$TO" --cache-control "'$CACHE_CONTROL'"
+        aws s3 sync "$FROM" "$TO" --cache-control "$CACHE_CONTROL"
     else
         aws s3 sync "$FROM" "$TO"
     fi
