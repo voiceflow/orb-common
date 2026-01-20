200b2341365a140821fefcecb669f77ca3acc965
theProf <theProf@gerggle.com>
2026-01-20 15:47:41 -0500

    chore(lint): prettier

--- src/scripts/track/check_exists.sh
+++ src/scripts/track/check_exists.sh
@@ -7,7 +7,7 @@ echo "${BUCKET?}"
 
 BRANCH_NAME="$CIRCLE_BRANCH"
 if [[ -z "$CIRCLE_BRANCH" && -n "$CIRCLE_TAG" ]]; then
-    BRANCH_NAME="master"
+  BRANCH_NAME="master"
 fi
 
 TRACK="tracks/$COMPONENT/$BRANCH_NAME"
@@ -19,12 +19,12 @@ set -e
 
 # Store the result on a file in tmp folder to use in future steps
 if [[ $SEARCH_TRACK_RESULT -eq 0 ]]; then
-    echo 'export TRACK_EXISTS="true"' > /tmp/TRACK_STATUS  # Track exists, skip following steps
+  echo 'export TRACK_EXISTS="true"' >/tmp/TRACK_STATUS # Track exists, skip following steps
 else
-    echo 'export TRACK_EXISTS="false"' > /tmp/TRACK_STATUS  # Track exists, skip following steps
-    if (( STOP )); then
-        curl --request POST \
-            --url "https://circleci.com/api/v2/workflow/$CIRCLE_WORKFLOW_ID/cancel" \
-            --header "Circle-Token: ${CIRCLECI_API_TOKEN}"
-    fi
+  echo 'export TRACK_EXISTS="false"' >/tmp/TRACK_STATUS # Track exists, skip following steps
+  if ((STOP)); then
+    curl --request POST \
+      --url "https://circleci.com/api/v2/workflow/$CIRCLE_WORKFLOW_ID/cancel" \
+      --header "Circle-Token: ${CIRCLECI_API_TOKEN}"
+  fi
 fi
